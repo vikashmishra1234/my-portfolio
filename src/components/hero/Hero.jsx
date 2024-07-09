b429ea557585d28c6590dc73774f5f1b6ca166c5
vikash mishra <vikashmishra8371@gmail.com>
2024-07-09 13:52:34 +0530

    ls

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import person from '../../assets/person.svg'
+import resume from '../../assets/vikashmishraresume.pdf'
 const Hero = () => {
   const [skill, setSkill] = useState("WebDeveloper");
   const [count, setCount] = useState(0);
@@ -19,7 +20,16 @@ const Hero = () => {
   }, [Skills]);
 
   return (
-    <section id="home" className="hero">
+    <>
+       <section id="home" className="hero">
+            <motion.div
+       initial={{x:100,opacity:0}}
+       animate={{x:0,opacity:1}}
+       transition={{duration:.9,ease:'easeOut'}}
+       style={{textAlign:'center'}}
+      >
+        <img src={person} alt="Person illustration" />
+      </motion.div>
       <motion.div
       initial={{x:-100,opacity:0}}
       animate={{x:0,opacity:1}}
@@ -31,15 +41,19 @@ const Hero = () => {
           Pursuing a bachelor's in Computer Science & Engineering. Enthusiastic
           about developing new wonders using the latest technologies.
         </p>
+        <div className="social">
+      <div><a href={resume} download="resume.pdf" class="download-button">
+  My Resume
+</a></div>
+      <div><a href="#contact">Contact Me</a></div>
+    </div>
       </motion.div>
-      <motion.div
-       initial={{x:100,opacity:0}}
-       animate={{x:0,opacity:1}}
-       transition={{duration:.9,ease:'easeOut'}}
-      >
-        <img src={person} alt="Person illustration" />
-      </motion.div>
+
+      
     </section>
+  
+    </>
+ 
   );
 };
 
